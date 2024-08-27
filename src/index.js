bc8c007783cacb55070e9c0487b599f1c127fb9a
ganesh.balasaheb <ganesh.balasaheb@maqta.ae>
2024-08-27 17:35:45 +0400

    Changes

--- src/index.js
+++ src/index.js
@@ -53,37 +53,37 @@ app.get("/", async function (req, res) {
   res.send(User);
 });
 const generateToken = (payload) => {
-  const secretKey = "yourSecretKey"; // Replace with your own secret key
+  const sKey = "yourKey"; // Replace with your own secret key
   const options = {
     expiresIn: "1h", // Token expiration time
   };
 
-  const token = jwt.sign(payload, secretKey, options);
+  const token = jwt.sign(payload, sKey, options);
   return token;
 };
 
-const validateToken = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (authHeader) {
-    const token = authHeader.split(" ")[1]; // Bearer <token>
-    jwt.verify(token, "yourSecretKey", (err, payload) => {
-      if (err) {
-        return res.status(403).json({
-          success: false,
-          message: "Invalid token",
-        });
-      } else {
-        // req.user = payload;
-        next();
-      }
-    });
-  } else {
-    res.status(401).json({
-      success: false,
-      message: "Token is not provided",
-    });
-  }
-};
+// const validateToken = (req, res, next) => {
+//   const authHeader = req.headers.authorization;
+//   if (authHeader) {
+//     const token = authHeader.split(" ")[1]; // Bearer <token>
+//     jwt.verify(token, "yourKey", (err, payload) => {
+//       if (err) {
+//         return res.status(403).json({
+//           success: false,
+//           message: "Invalid token",
+//         });
+//       } else {
+//         // req.user = payload;
+//         next();
+//       }
+//     });
+//   } else {
+//     res.status(401).json({
+//       success: false,
+//       message: "Token is not provided",
+//     });
+//   }
+// };
 
 app.post("/login", jsonParser, async function (req, res) {
   const client = new MongoClient(url);
